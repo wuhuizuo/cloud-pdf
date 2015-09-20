03da0faca57cc6a818ed7d7ce4857d0ffc57cbea
wuhuizuo <wuhuizuo@126.com>
2015-09-20 06:53:23 +0000

    add books dir

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ var app = express();
 app.listen(process.env.PORT, process.env.IP, function() {
   console.log('Server started on ' + process.env.IP + ':' + process.env.PORT)
 });
-app.use(express.static('static'));
+app.use('/viewer', express.static('viewer'));
+
+app.get('/', function(req, res){
+  res.send('Index page!'); 
+});
